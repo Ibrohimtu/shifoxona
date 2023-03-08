e3ccbac3215130098730c7abbefa27264467c99a
ibrohim <turdaliyevibrohim35@gmail.com>
2023-03-08 11:46:05 +0500

    Initial commit

---
+++ src/main/java/uz/brogrammers/shifoxona/ShifoxonaApplication.java
@@ -0,0 +1,13 @@
+package uz.brogrammers.shifoxona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifoxonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifoxonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/brogrammers/shifoxona/ShifoxonaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brogrammers.shifoxona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifoxonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
